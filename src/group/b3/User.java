8e9705684abaccf892fad16afa4ded818f9c9840
Henrik Vik <henkvik@gmail.com>
2016-05-12 13:52:04 +0200

    user class

---
+++ src/group/b3/User.java
@@ -0,0 +1,25 @@
+package group.b3;
+
+/**
+ * Created by henkv on 2016-05-12.
+ */
+public abstract class User {
+    private String name;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
