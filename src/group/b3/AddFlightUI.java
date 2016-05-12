160c20497ebd43ddc2f2591d8dc0b51dbae77087
walter19 <walter.karlqvist@gmail.com>
2016-05-12 14:49:15 +0200

    UI end

---
+++ src/group/b3/AddFlightUI.java
@@ -0,0 +1,11 @@
+package group.b3;
+
+/**
+ * Created by Walter_2 on 2016-05-12.
+ */
+public class AddFlightUI extends UI{
+    public void addFlight()
+    {
+
+    }
+}
--- src/group/b3/SearchFlightUI.java
+++ src/group/b3/SearchFlightUI.java
@@ -1,10 +1,12 @@
 package group.b3;
 
+import java.util.ArrayList;
+
 /**
  * Created by Walter_2 on 2016-05-12.
  */
 public class SearchFlightUI extends UI {
-    public void showFlights(List<Flights>) {
+    public void showFlights(ArrayList<Flight> list) {
 
     }
 }
