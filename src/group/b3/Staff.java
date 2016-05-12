017344f81eb0e049f37bdd66ac4018c91c4200b7
HannahLovberg <hannah.lovberg@live.com>
2016-05-12 14:09:07 +0200

    Created Staff class

---
+++ src/group/b3/Staff.java
@@ -0,0 +1,17 @@
+package group.b3;
+
+/**
+ * Created by hannahlovberg on 2016-05-12.
+ */
+public class Staff {
+    private float salary;
+
+    public float getSalary(){
+        return salary;
+    }
+
+    public void setSalary(float salary){
+        salary = salary;
+    }
+
+}
