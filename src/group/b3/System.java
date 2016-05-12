96919a20240a7e329e292016e8a2c4ea899e521f
HannahLovberg <hannah.lovberg@live.com>
2016-05-12 14:27:19 +0200

    Edited System

--- src/group/b3/System.java
+++ src/group/b3/System.java
@@ -4,7 +4,15 @@ package group.b3;
  * Created by hannahlovberg on 2016-05-12.
  */
 public class System {
+    private ArrayList<User> users;
 
+    public System(ArrayList<User> users) {
+        this.users = users;
+    }
 
+    public User getUser(String username, String password){
+        for (int i= 0; i < users.size(); i++){
 
+        }
+    }
 }
