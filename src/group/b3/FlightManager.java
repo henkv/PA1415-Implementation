97d1280d867e4eb70d1e1a797096b01e6e2f1441
HannahLovberg <hannah.lovberg@live.com>
2016-05-15 19:01:45 +0200

    Edited FlightManager

--- src/group/b3/FlightManager.java
+++ src/group/b3/FlightManager.java
@@ -1,5 +1,6 @@
 package group.b3;
 import java.util.ArrayList;
+import java.util.List;
 
 public class FlightManager {
     public ArrayList<Flight> flights;
@@ -7,5 +8,23 @@ public class FlightManager {
     public void addFlight(Flight flight){
         flights.add(flight);
     }
+
+    public ArrayList<Flight> serachFlights(String destination, String origin){
+        ArrayList<Flight> availableFlights = new ArrayList<Flight>();
+
+        for (int i = 0; i < flights.size(); i++){
+
+            Flight testFlight = flights.get(i);
+
+            if(testFlight.getDestination() == destination && testFlight.getOrigin() == origin){
+                availableFlights.add(testFlight);
+            }
+        }
+
+
+        return availableFlights;
+
+    }
+
 
 }
