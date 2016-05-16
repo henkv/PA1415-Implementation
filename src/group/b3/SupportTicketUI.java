42f3792aef3300cd5033822e87529170bfd47763
walter19 <walter.karlqvist@gmail.com>
2016-05-16 15:44:47 +0200

    Support ticket ui and stuff

--- src/group/b3/SupportTicketUI.java
+++ src/group/b3/SupportTicketUI.java
@@ -6,10 +6,27 @@ import javax.swing.*;
 public class SupportTicketUI extends UI{
 
     private JPanel contentPanel;
+    private JButton backButton;
+    private JButton submitButton;
+    private JFormattedTextField ticket;
 
     public SupportTicketUI()
+
     {
-        /*backButton.addActionListener(e -> back());*/
+        String save = "HEJEJEJ";
+        submitButton.addActionListener(e -> confirm());
+        backButton.addActionListener(e -> back());
+        ticket.setText(save);
+    }
+
+    public void back()
+    {
+
+    }
+
+    public void confirm()
+    {
+
     }
 
     @Override
